e9623b91940f96db18e3c1d85172d1b6e349803b
Liam G <liamgallagher094@gmail.com>
2025-05-22 22:45:58 +0100

    Introduction

---
+++ Start.py
@@ -0,0 +1,9 @@
+import time 
+
+print ("-------------OUTSIDE THE BOX----------------")
+time.sleep(1)
+user = input ("Please emter your username ")
+time.sleep(2)
+print("Welcome to Outside The Box " + user + "!")
+time.sleep(1)
+
