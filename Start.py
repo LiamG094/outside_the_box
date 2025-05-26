03b7d91fe6aea4730625dd3f365aaf4ced337a4b
Liam G <liamgallagher094@gmail.com>
2025-05-26 20:58:13 +0100

    Proof read, spelling and grammar corrected

--- Start.py
+++ Start.py
@@ -1,9 +1,11 @@
 import time 
+
 #starting prompt to get the username
+
 print ("-------------OUTSIDE THE BOX----------------")
 time.sleep(1)
-user = input ("Please emter your username ")
+user = input ("Please enter your username ")
 time.sleep(2)
-print("Welcome to Outside The Box " + user + "!")
+print("Welcome to Outside The Box, " + user + "!")
 time.sleep(1)
 
