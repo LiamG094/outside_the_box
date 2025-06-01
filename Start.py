6a9cd342561e542fbd82bd2b5c93de13a2991205
Liam G <liamgallagher094@gmail.com>
2025-06-01 21:13:03 +0100

    Main Menu function

--- Start.py
+++ Start.py
@@ -3,18 +3,26 @@ import time
 
 print ("----------OUTSIDE THE BOX----------")
 time.sleep(1)
-user = input ("Please enter your username ")
-time.sleep(2)
-print("Welcome to Outside The Box, " + user + "!")
-time.sleep(2)
 
-#User selects an option from the main menu to proceed
+time.sleep(1)
 
-main_menu = ("----------MAIN MENU----------") # could this be an object ??? 
-print (main_menu)
-print("HELP - instructions ")
-print("START - start the game ")
-option = input("Select an option: ")
+
+def main_menu():
+    user = input ("Please enter your username ")
+    print("Welcome to Outside The Box, " + user + "!")
+    time.sleep(1)
+    print("Type HELP for instructions and START to begin.")
+    
+    option = input("What would you like to do? ").strip().upper()  # Clean input
+    
+    if option == "HELP":
+        print("Here are the instructions...")
+    elif option == "START":
+        print("Starting the game...")
+    else:
+        print("Please enter a valid option.")
+
+main_menu()
 
 #if option is "HELP" print the instructions
 #if options is START - Print game intro 
